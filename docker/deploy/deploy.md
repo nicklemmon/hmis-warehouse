336c325aef8e24ac1834414249d035d3cd240860
Todd Blackman <tdblackman@gmail.com>
2020-01-17 21:22:26 -0500

    environment for pods

--- docker/deploy/deploy.md
+++ docker/deploy/deploy.md
@@ -1,9 +1,8 @@
 # Deployment
 
-
-
 ## Build and push an image to repository
 
+        export AWS_PROFILE=openpath
         export VERSION=0.0.3
         export REGISTRY_ID=`aws ecr describe-repositories | jq '.repositories[0].registryId' | sed -e 's/"//g'`
         export REPO=$REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com/open-path-warehouse
---
+++ docker/deploy/make_secrets.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'base64'
+
+namespace = ARGV[0]
+filename = "#{namespace}.env"
+
+if !File.exists?(filename)
+  raise "You must have <namespace>.env file"
+end
+
+print(<<EOS)
+apiVersion: v1
+kind: Secret
+metadata:
+  name: #{namespace}-env
+data:
+EOS
+
+File.open(filename) do |f|
+  f.each_line do |line|
+    if delimit_offset = line.index('=')
+      key = line[0..delimit_offset-1]
+      val = line[delimit_offset+1..-1].chomp
+
+      if val.length == 0
+        val = "''"
+      end
+
+      encoded_val = Base64.strict_encode64(val)
+      puts "  #{key}: #{encoded_val}"
+    end
+  end
+end
