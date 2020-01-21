7a1d45d092fd74d4957f69eaa182a709572a9c51
Todd Blackman <tdblackman@gmail.com>
2020-01-21 15:45:47 -0500

    wip

--- docker/deploy/build.sh
+++ docker/deploy/build.sh
@@ -11,15 +11,10 @@ git rev-parse HEAD > REVISION
 docker build \
   --file=./Dockerfile \
   --tag open-path-warehouse:latest \
-  ../.. || exit
+  ../..
 
 echo log in to ECR: Elastic Container Registry
 $(aws ecr get-login --no-include-email --region us-east-1)
-
-#export REGISTRY_ID=`aws ecr describe-repositories | jq '.repositories[0].registryId' | sed -e 's/"//g'`
-#export REPO=$REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com/open-path-warehouse
-#export IMAGE=${REPO}:${VERSION}
-#echo using $IMAGE as the destination
 
 echo tagging this last build
 docker tag open-path-warehouse:latest $IMAGE
--- docker/deploy/deploy.md
+++ docker/deploy/deploy.md
@@ -9,6 +9,7 @@
         echo -n -- >> .version
         git rev-parse --short HEAD | tr -d '\n' >> .version
         export VERSION=$(cat .version)
+        echo $VERSION
 
         export REGISTRY_ID=`aws ecr describe-repositories | jq '.repositories[0].registryId' | sed -e 's/"//g'`
         export REPO=$REGISTRY_ID.dkr.ecr.us-east-1.amazonaws.com/open-path-warehouse
