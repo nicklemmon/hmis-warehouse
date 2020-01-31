6d01c1bf44c28b446572443baf744b977be2298c
Todd Blackman <tdblackman@gmail.com>
2020-01-31 14:03:12 -0500

    wip

--- docker/deploy/build.sh
+++ docker/deploy/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+# abort on first error
+set -e
+
 ./set_version.sh
+
+
+echo WIP: abort if git not clean
 
 export AWS_PROFILE=openpath
 
@@ -15,8 +21,8 @@ docker build \
   --tag open-path-warehouse:latest \
   ../..
 
-echo log in to ECR: Elastic Container Registry
-$(aws ecr get-login --no-include-email --region us-east-1)
+#echo log in to ECR: Elastic Container Registry
+#$(aws ecr get-login --no-include-email --region us-east-1)
 
 echo tagging this last build
 docker tag open-path-warehouse:latest $IMAGE
@@ -26,7 +32,8 @@ docker image ls -f 'label=host.openpath.app=open-path-warehouse'
 echo Push image to repository.
 echo This is supposed to fail if you forgot to change the version from a previous push
 echo The first push after big changes may be slow
-docker push $IMAGE
+#docker push $IMAGE
+ecs-cli push --ecs-profile=openpath $IMAGE
 
 echo clean up
 docker image prune
